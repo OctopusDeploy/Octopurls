3aebfc9df73b1356f32260625647538712f083cc
Robert Wagner <robert.wagner@octopus.com>
2021-02-18 15:01:35 +1000

    Switched back to mainline versioning so that it enthusiastically deploys

---
+++ source/Octopurls.Tests/VersionTests.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Octopurls.Tests
+{
+    public class VersionTests
+    {
+        [Test]
+        public void MainlineVersioningIsUsed()
+        {
+            typeof(Startup).Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?
+                .InformationalVersion
+                .Should()
+                .NotContain("-ci", "Builds from `main` should have non-prerelease version numbers so that it gets deployed to production automatically.");
+        }
+    }
+}
