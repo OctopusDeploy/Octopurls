079918a240bf9713c38793d9442c1cea4d55979e
Robert Wagner <robert.wagner@octopus.com>
2021-02-18 15:13:35 +1000

    Fixed text

--- source/Octopurls.Tests/VersionTests.cs
+++ source/Octopurls.Tests/VersionTests.cs
@@ -12,7 +12,7 @@ namespace Octopurls.Tests
             typeof(Startup).Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?
                 .InformationalVersion
                 .Should()
-                .NotContain("-ci", "Builds from `main` should have non-prerelease version numbers so that it gets deployed to production automatically.");
+                .NotContain("-ci", "Builds from `master` should have non-prerelease version numbers so that it gets deployed to production automatically.");
         }
     }
 }
